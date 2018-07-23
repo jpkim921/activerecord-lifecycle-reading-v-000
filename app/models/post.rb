36643ecc63e81c0ee119810fcca796cc82cb6968
Jayon Kim <jpkim921@gmail.com>
2018-07-23 19:20:57 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,14 +7,14 @@ class Post < ActiveRecord::Base
   before_validation :make_title_case 
   
   # New Code!!
-  before_save :email_author_about_post
+  # before_save :email_author_about_post
 
   
   # Here is a rule of thumb: Whenever you are modifying an attribute of the model, use before_validation. If you are doing some other action, then use before_save.
 
 
   # New Code!!
-  before_create :some_method
+  # before_create :some_method
 
   # Difference between before_save and before_create is the latter gets called only when a new object is being created
 
